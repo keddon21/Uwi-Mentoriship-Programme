62c5155b7edcb859a9ccf8c086b629b26e796af9
keddon21 <55594405+keddon21@users.noreply.github.com>
2019-11-25 16:45:55 -0500

    Add files via upload

---
+++ Apply.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UWI Mentorship Programme</title>
+
+	<link rel="stylesheet" type="text/css" src="Style.css">
+	<meta charset="utf-8">
+	<meta name = "author" content = "Gabie and The Boys">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+</head>
+	
+
+
+	<header id="banner">
+		<div class="brand">
+			<p>Mentorship Programme</p>
+		</div>
+
+	
+	</header>
+
+	<div id= "homeOptions" class="homeOptions">
+		<ul>
+			<li>
+				<a href="/Mentor">Apply to be a mentor</a>
+			</li>
+			
+		</ul>
+
+	</div>	
+<body>
+
+</body>
+</html>
